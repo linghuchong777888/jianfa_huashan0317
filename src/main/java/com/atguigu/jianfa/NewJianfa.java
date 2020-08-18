a30951a8ba50871368b11bcb58bf402c791638e9
linghuchong <linghuchong777888@126.com>
2020-08-18 16:46:59 +0800

    新剑法

---
+++ src/main/java/com/atguigu/jianfa/NewJianfa.java
@@ -0,0 +1,10 @@
+package com.atguigu.jianfa;
+
+public class NewJianfa {
+    public static void main(String[] args) {
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+    }
+}
