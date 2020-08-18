3d2942c7b5238c932e6238b96435b0b458bca838
linghuchong <linghuchong777888@126.com>
2020-08-18 15:55:08 +0800

    linghuchong

--- src/main/java/com/atguigu/jianfa/Dugu.java
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -5,10 +5,10 @@ public class Dugu {
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
-        System.out.println("紫霞神功好");
-        System.out.println("紫霞神功好");
-        System.out.println("紫霞神功好");
-        System.out.println("紫霞神功好");
+        System.out.println("linghuchong紫霞神功好");
+        System.out.println("linghuchong紫霞神功好");
+        System.out.println("linghuchong紫霞神功好");
+        System.out.println("linghuchong紫霞神功好");
 
     }
 }
