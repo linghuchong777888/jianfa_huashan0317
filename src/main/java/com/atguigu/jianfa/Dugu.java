92d3260ddb00e4485e4076b51fe444f482b11d96
yuebuqun <yuebuqun777888@126.com>
2020-08-18 15:43:15 +0800

    紫霞神功

--- src/main/java/com/atguigu/jianfa/Dugu.java
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -5,5 +5,10 @@ public class Dugu {
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
+        System.out.println("紫霞神功好");
+        System.out.println("紫霞神功好");
+        System.out.println("紫霞神功好");
+        System.out.println("紫霞神功好");
+
     }
 }
